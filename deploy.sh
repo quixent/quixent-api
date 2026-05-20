80127397d13f6c92a3c82e7fd07bcb103868e80f
Murugenthiran <murugenthiranme@gmaile.com>
2026-05-21 00:21:12 +0530

    Docker files added for depolyment

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME="Quixent-api"
+CONTAINER_NAME="Quixent-api-container"
+ENV_FILE=".env"
+HOST_PORT=5002
+CONTAINER_PORT=5002
+
+echo "🔍 Checking .env file..."
+if [ ! -f "$ENV_FILE" ]; then
+  echo "❌ .env file not found!"
+  exit 1
+fi
+
+echo "🛑 Stopping existing container..."
+docker stop $CONTAINER_NAME 2>/dev/null || true
+docker rm $CONTAINER_NAME 2>/dev/null || true
+
+echo "🚀 Building Docker image..Quixent API."
+docker build -t $IMAGE_NAME .
+
+echo "▶️ Running container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  --env-file $ENV_FILE \
+  -p $HOST_PORT:$CONTAINER_PORT \
+  --restart unless-stopped \
+  $IMAGE_NAME
+
+echo "✅ Deployment successful!"
