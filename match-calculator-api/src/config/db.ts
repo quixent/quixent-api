abbecabecfcff7cbacf4e28165b9efb87c4a3731
Murugenthiran <murugenthiranme@gmaile.com>
2026-05-21 12:14:13 +0530

    DB related updated

--- match-calculator-api/src/config/db.ts
+++ match-calculator-api/src/config/db.ts
@@ -3,8 +3,8 @@ import mongoose, { Connection } from 'mongoose';
 export let matchDb: Connection;
 
 export const connectMatchDB = async (): Promise<void> => {
-  const mongoUri = process.env.MATCH_MONGO_URI;
-  if (!mongoUri) throw new Error('MATCH_MONGO_URI is not set in .env');
+  const mongoUri = process.env.APPS_MONGO_URI;
+  if (!mongoUri) throw new Error('APPS_MONGO_URI is not set in .env');
   matchDb = mongoose.createConnection(mongoUri);
   await matchDb.asPromise();
   console.log('✅ Match MongoDB connected');
